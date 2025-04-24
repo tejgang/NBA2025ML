e6db9a02448cee6274ab4398ca071b2a93603070
Trevor Hicks <trev23hix@gmail.com>
2025-04-24 15:39:54 -0700

    predit2.py rf game by game

--- rf_game_by_game/predict2.py
+++ rf_game_by_game/predict2.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
-from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 from imblearn.over_sampling import SMOTE
-import warnings
+from imblearn.ensemble import BalancedRandomForestClassifier
+from lightgbm import LGBMClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 
 np.random.seed(43)
 print("loading that dih...")
@@ -17,8 +17,30 @@ print(nba_df.head())
 
 missing_values = nba_df.isnull().sum()
 print(missing_values[missing_values > 0])
+nba_df.drop(['PTS', 'PTS.1', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12'], axis=1, inplace=True)
 
+
+
+nba_df["Vis_Win"] = nba_df["Win"]
+nba_df["Home_Win"] = nba_df["Win.1"]
+nba_df.drop(['Win','Win.1',"Vis_Win"],axis=1,inplace=True)
+print(nba_df.head())
 
 features = [
-    'Visitor/Neutral','PTS','Win','Home/Neutral','PTS','Win','Visitor Seed','Home Seed','Year'
-]
+    'Home_Win','Visitor Seed','Home Seed']
+
+df_train = nba_df[nba_df['season_end'] <= 2001].copy()
+df_predict = nba_df[nba_df['season_end'] == 2025].copy()
+
+X = df_train[features].copy()
+y = df_train['Home_Win']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+model = BalancedRandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+print("Train Accuracy:", model.score(X_train, y_train))
+y_pred = model.predict(X_test)
+print(f'Test Accuracy: {roc_auc_score(y_test, y_pred):.5f}')
+
