a20ab357dc0714530243e420a0190c301992bd3d
Trevor Hicks <trev23hix@gmail.com>
2025-04-26 15:31:24 -0700

    fixed up randomforrest_gbg.py - can toggle between tuned model and reg model

--- mote_carlo_sim/montecarlo.py
+++ mote_carlo_sim/montecarlo.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-from rf_game_by_game import predict2 as rf_gbg
+from rf_game_by_game import randomforrest_gbg as dih
 
 '''
 This will be a Monte Carlo Simulation for the rf game by game
 
 '''
+
