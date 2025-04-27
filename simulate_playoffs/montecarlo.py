395998f0305f1506e826f514cc669a57868fa6a5
Tej Gangupantula <tejgang600@gmail.com>
2025-04-27 13:55:36 -0700

    create playoffs series csv

--- simulate_playoffs/montecarlo.py
+++ simulate_playoffs/montecarlo.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from rf_game_by_game import randomforrest_gbg as dih
+from xbg_game_by_game import predict_xgb as dih2
 
 '''
 This will be a Monte Carlo Simulation for the rf game by game
